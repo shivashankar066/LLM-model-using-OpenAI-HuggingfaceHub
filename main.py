cf827b93e08450f78e1d4ad1d8ed5289c1907fd3
Shivashankar Rampur <84033410+shivashankar066@users.noreply.github.com>
2023-12-12 15:09:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,7 @@ import streamlit as st
 os.environ["OPENAI_API_KEY"] = openai_key
 st.title("Author: Shivashankar, LangChain demo with OpenAI")
 input_text = st.text_input("Search, what you want!")
-
-
 llm=OpenAI(temperature= 0.8)
 
 if input_text:
-    st.write(llm(input_text))
-
+    st.write(llm(input_text))  
